d9cd7ab36bd5291b1d135446304dc4968486b731
hoodbos <realityabiola10@gmail.com>
2023-02-17 14:53:01 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - this is main function define as void
+ *
+ * Return: the output is 0 if successfu
+ */
+
+int main(void)
+
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
