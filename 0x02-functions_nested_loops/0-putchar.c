e0bcb73e592218b553a9ee8e5e13547bdd6e1efc
hoodbos <realityabiola10@gmail.com>
2023-02-22 17:55:51 -0800

    putchar alphabet

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main headers function
+ * _putchar: print the _putchar value
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('a');
+	return (0);
+}
