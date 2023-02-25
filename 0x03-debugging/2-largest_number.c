a3daf8cb4e5c0379d56b3bdba991ff16667c7338
hoodbos <realityabiola10@gmail.com>
2023-02-24 17:46:44 -0800

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -24,11 +24,11 @@ int largest_number(int a, int b, int c)
 	}
 	else if (b > c && c > a)
 		largest = b;
-	else if (c > b && b > a)
+	else if (c > a && a > b)
 	{
 		largest = c;
 	}
-	else if (c > a && a > b)
+	else if (c > b && b > a)
 		largest = c;
 	return (largest);
 }
