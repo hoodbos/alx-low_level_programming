fc15dec24f651e91db2ac97dc5d6844e23bb86b0
hoodbos <realityabiola10@gmail.com>
2023-02-27 05:35:27 -0800

    uppercase letter

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - declaration of function
+ * description: check case of c
+ * @c: character to test
+ * Return: 1 if character is true else return 0 if character is false
+ */
+
+int _isupper(int c);
+{
+	if (c >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
