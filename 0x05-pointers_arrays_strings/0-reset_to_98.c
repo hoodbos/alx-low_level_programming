a20a9f1e6357738fbcc1f65e588fef1248a6a080
hoodbos <realityabiola10@gmail.com>
2023-03-14 09:46:51 -0700

    updating pointer pointer value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - change or reset value of pointer
- * the pointer pint to 98
- * @n: always 0
+ * reset_to_98 - change or reset value of pointer
+ * @n: pointer piont to 98
+ * return: always 0
  */
 void reset_to_98(int *n)
 {
